330fc8a110b277c4b92e0d58a2e76520146c1e5b
Alexis Esperilla <esperilla_castro@outlook.com>
2026-06-17 01:28:02 -0600

    Otro cambio menor

--- scripts/red.sh
+++ scripts/red.sh
@@ -23,7 +23,7 @@ if [ ! -f "$CONFIG_FILE" ]; then
 fi
 
 if [ ! -f "$CONFIG_FILE" ]; then
-    echo "No se encontró config.txt. Crea $CONFIG_FILE"
+    echo "No se encontró config.txt, crea $CONFIG_FILE"
     exit 1
 fi
 
